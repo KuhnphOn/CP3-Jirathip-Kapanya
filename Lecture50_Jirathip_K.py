2f5f3880aa4e3c0db63d7ee1c2604587a56db23b
KuhnphOn <88961962+KuhnphOn@users.noreply.github.com>
2022-06-05 13:14:29 +0700

    Add files via upload

---
+++ Lecture50_Jirathip_K.py
@@ -0,0 +1,24 @@
+def add_number(x, y):
+    print("x+y =", x+y)
+
+
+def minus_number(x, y):
+    print("x-y =", x-y)
+
+
+def multiply_number(x, y):
+    print("x*y =", x*y)
+
+
+def divide_number(x, y):
+    print("x/y =", x/y)
+
+
+def combination(x, y):
+    add_number(x, y)
+    minus_number(x, y)
+    multiply_number(x, y)
+    divide_number(x, y)
+
+
+combination(5, 10)
